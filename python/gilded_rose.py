e2dcc8e678ad124c3c730f5fb610ff364f86add2
Yattioui-Visuals <a.yattioui@gmail.com>
2024-10-30 06:50:09 +0100

    Improve Code

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,14 @@
 # -*- coding: utf-8 -*-
+SULFURAS_QUALITY = 80
+MAX_QUALITY = 50
+MIN_QUALITY = 0
 
-class GildedRose(object):
 
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+class Names:
+    BACKSTAGE_PASS = 'Backstage passes to a TAFKAL80ETC concert'
+    SULFURAS = 'Sulfuras, Hand of Ragnaros'
+    AGED_BRIE = 'Aged Brie'
+    CONJURED = 'Conjured'
 
 
 class Item:
@@ -44,3 +19,60 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class GildedRose(object):
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            if item.name == Names.SULFURAS:
+                self.update_sulfuras(item)
+            elif item.name == Names.AGED_BRIE:
+                self.update_aged_brie(item)
+            elif item.name == Names.BACKSTAGE_PASS:
+                self.update_backstage_pass(item)
+            elif item.name == Names.CONJURED:
+                self.update_conjured_item(item)
+            else:
+                self.update_standard_item(item)
+
+    def update_sulfuras(self, item):
+        item.quality = SULFURAS_QUALITY
+
+    def update_aged_brie(self, item):
+        self.increase_quality(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self.increase_quality(item)
+
+    def update_backstage_pass(self, item):
+        self.increase_quality(item)
+        if item.sell_in < 11:
+            self.increase_quality(item)
+        if item.sell_in < 6:
+            self.increase_quality(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = MIN_QUALITY
+
+    def update_conjured_item(self, item):
+        self.decrease_quality(item, 2)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self.decrease_quality(item, 2)
+
+    def update_standard_item(self, item):
+        self.decrease_quality(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self.decrease_quality(item)
+
+    def increase_quality(self, item):
+        if item.quality < MAX_QUALITY:
+            item.quality += 1
+
+    def decrease_quality(self, item, amount=1):
+        item.quality = max(MIN_QUALITY, item.quality - amount)
+
